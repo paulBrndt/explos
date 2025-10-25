d27136bb300a384d52fa89c3c5e5a1964ad5b52f
Paul <>
2025-10-25 13:43:20 +0200

    moved videos to code

--- code/info-theory/main.py
+++ code/info-theory/main.py
@@ -10,18 +10,18 @@ class WordleScene(Scene):
     def construct(self):
         self.setup()
         self.add_grid()
+        self.embed()
         
         
     def add_grid(self):
         buff = 0.1
         row = Square(side_length=1).get_grid(1, self.n_letters, buff=buff)
         grid = row.get_grid(6, 1, buff=buff)
-        grid.set_height(1)
+        grid.set_height(5)
         grid.move_to(ORIGIN)
         grid.set_stroke(WHITE, 2)
         grid.words = VGroup()
         grid.pending_word = VGroup()
-        #grid.add(grid.words, grid.pending_word)
         grid.pending_pattern = None
         grid.add_updater(lambda m: m)
         self.grid = grid
